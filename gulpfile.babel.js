9dc8093180b135c1906c1d2a62c8a3756b3c7c8d
Aaron <git@aharding.co.uk>
2016-09-09 15:15:58 +0200

    🎉

---
+++ gulpfile.babel.js
@@ -0,0 +1,26 @@
+import bsConfig  from 'gulp-bootstrap-configurator'
+import gulp from 'gulp'
+
+const config = './config.json'
+const dir = './build/' + Date.now() + '/'
+
+// For CSS
+// It will create `bootstrap.css` in directory `assets`.
+gulp.task('css', () => {
+  return gulp.src(config)
+    .pipe(bsConfig.css({ compress: true }))
+    .pipe(gulp.dest(dir))
+})
+
+// For JS
+// It will create `bootstrap.js` in directory `assets`.
+gulp.task('js', () => {
+  return gulp.src(config)
+    .pipe(bsConfig.js({ compress: true }))
+    .pipe(gulp.dest(dir))
+})
+
+gulp.task('all', () => {
+	gulp.run('css')
+	gulp.run('js')
+})
